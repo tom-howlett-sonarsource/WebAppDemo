204b9112a74b4e95fd53ca98ba29d5afebcb5984
Tom Howlett <tom.howlett@sonarsource.com>
2024-05-09 17:52:42 +0100

    more attempts

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -83,6 +83,19 @@ public class IndexModel : PageModel
     }
 }
 
+    public void Encrypt(byte[] key, byte[] dataToEncrypt, MemoryStream target)
+    {
+        var aes = new AesCryptoServiceProvider();
+
+        byte[] iv     = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+        var encryptor = aes.CreateEncryptor(key, iv); // Noncompliant
+
+        var cryptoStream = new CryptoStream(target, encryptor, CryptoStreamMode.Write);
+        var swEncrypt    = new StreamWriter(cryptoStream);
+
+        swEncrypt.Write(dataToEncrypt);
+    }
+
     public IActionResult OnPost()
     {
         /*if(TextInput != null)
