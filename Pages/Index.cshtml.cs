7166b7b3bb909b61375fc20245a7efc27a4ffdfa
Tom Howlett <tom.howlett@sonarsource.com>
2024-05-22 11:05:11 -0700

    Added driving licence check

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,61 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
-namespace WebAppDemo.Pages;
-
-public class IndexModel : PageModel
+namespace WebAppDemo.Pages
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly ILogger<IndexModel> _logger;
 
-    public void OnGet()
-    {
+        [BindProperty]
+        [Required(ErrorMessage = "Driving Licence Number is required")]
+        public string DrivingLicenceNumber { get; set; }
 
+        [BindProperty]
+        [Required(ErrorMessage = "Expiry Date is required")]
+        public DateTime ExpiryDate { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                if (!ValidateDrivingLicence(DrivingLicenceNumber))
+                {
+                    ModelState.AddModelError("DrivingLicenceNumber", "Invalid UK Driving Licence Number");
+                }
+
+                if (!ValidateExpiryDate(ExpiryDate))
+                {
+                    ModelState.AddModelError("ExpiryDate", "Expiry Date should be at least 6 months from now");
+                }
+            }
+        }
+
+        private bool ValidateDrivingLicence(string licenceNumber)
+        {
+            // UK Driving Licence Number regex
+            var regex = new Regex(@"^(?=.*[A-Z])(?=.*\d)[A-Z\d]{16}$");
+            return regex.IsMatch(licenceNumber);
+        }
+
+        private bool ValidateExpiryDate(DateTime expiryDate)
+        {
+            // Check if the expiry date is at least 6 months from now
+            return expiryDate > DateTime.Now.AddMonths(6);
+        }
     }
 }
